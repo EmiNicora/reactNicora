4210496136e1988959ad90788909c3911c91df9e
EmiNicora <86694310+EmiNicora@users.noreply.github.com>
2021-12-21 16:13:37 -0300

    Add files via upload

---
+++ nicora_app/src/mock/Mockeditems.jsx
@@ -0,0 +1,28 @@
+const MockedItems = [
+    {
+		title: "Varita Harry Potter",
+		descripcion: "36cm de largo, hecha con pluma de fenix",
+		precio: 3000,
+	    imagen: "https://cdn.shopify.com/s/files/1/0514/6332/3817/products/1230211-1_large.png?v=1626866265",
+    },
+    {
+    title: "Producto 1",
+    descripcion: "sfdf",
+    precio: 500,
+    imagen: "https://cdn.shopify.com/s/files/1/0514/6332/3817/products/1331356-1_large.png?v=1621854127Harry2_grande.png"
+    },
+    {
+    title: "Producto 2",
+    descripcion: "sfdf",
+    precio: 500,
+    imagen: "https://cdn.shopify.com/s/files/1/0514/6332/3817/products/1331356-1_large.png?v=1621854127Harry2_grande.png"
+    },
+    {
+    title: "Producto 3",
+    descripcion: "sfdf",
+    precio: 500,
+    imagen: "https://cdn.shopify.com/s/files/1/0514/6332/3817/products/1331356-1_large.png?v=1621854127Harry2_grande.png"
+    },
+];
+
+export default MockedItems;
